3d5cac54996b8a3f5b05c45ead16a13d86bfd8a9
Mirabbosbek9778 <jwicj969@gmail.com>
2023-11-06 15:25:15 +0500

    full project cards page home root

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,8 +3,23 @@ import Filter from "../../components/generic/filter/Filter";
 import Header from "../../components/header/Header";
 import { dataCards } from "../../mock/dataCard";
 import { Container, Content, Wrapper } from "../../assets/styleHome";
+// import { useState, useEffect } from "react";
 
 const Home = () => {
+  // const [data, setData] = useState([]);
+
+  // useEffect(() => {
+  //   fetch("https://0001.uz/books")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       setData(data);
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error:", error);
+  //     });
+  // }, []);
+  // console.log(data);
+
   return (
     <Wrapper>
       <Container>
